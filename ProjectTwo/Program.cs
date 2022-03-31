6e0ae28d621b7ad7518f914639453fcf34639e69
mhtaldmr <mht.alidemir@gmail.com>
2022-03-31 19:11:01 +0300

    Add project files.

---
+++ ProjectTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
